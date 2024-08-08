8cff7d9e45ab5dc83724e111bd60a52d6b1f8548
stevexero <steveanthony999@gmail.com>
2024-08-08 13:51:42 -0700

    Updated screens

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-// import Scanner from './Scanner';
+import { isMobile } from 'react-device-detect';
+import { GoHomeFill, GoTriangleLeft } from 'react-icons/go';
 import Scanner2 from './Scanner2';
 import useBarCodeStore from './store';
 
@@ -9,12 +10,62 @@ function App() {
   const [screen, setScreen] = useState('main');
   const [title, setTitle] = useState('BIM');
   const [subTitle, setSubTitle] = useState('BoxValet Inventory Management');
+  const [history, setHistory] = useState(['main']);
 
   const handleClick = (e) => {
-    setScreen(e.target.id);
-    let words = e.target.name.split(',');
-    setTitle(words[0]);
-    setSubTitle(words[1]);
+    const newScreen = e.target.id;
+    if (e.target.name) {
+      const words = e.target.name.split(',');
+      setTitle(words[0]);
+      setSubTitle(words[1]);
+    }
+    setHistory([...history, newScreen]);
+    setScreen(newScreen);
+  };
+
+  const handleHomeClick = () => {
+    setTitle('BIM');
+    setSubTitle('BoxValet Inventory Management');
+    setHistory(['main']);
+    setScreen('main');
+  };
+
+  const handleBackClick = () => {
+    const newHistory = [...history];
+    newHistory.pop();
+    const previousScreen = newHistory[newHistory.length - 1];
+    setHistory(newHistory);
+    setScreen(previousScreen);
+  };
+
+  const HomeButton = () => {
+    return (
+      <div className='absolute top-4 right-4'>
+        <button
+          id='main'
+          className='btn btn-circle btn-error text-white'
+          onClick={handleHomeClick}
+        >
+          <GoHomeFill className='text-white' size='2rem' />
+        </button>
+      </div>
+    );
+  };
+
+  const BackButton = () => {
+    return (
+      <div className='absolute top-4 left-4'>
+        <button
+          className='btn btn-circle btn-error text-white'
+          onClick={handleBackClick}
+        >
+          <GoTriangleLeft
+            className='text-white mr-[6px] -mt-[1px]'
+            size='3rem'
+          />
+        </button>
+      </div>
+    );
   };
 
   return (
@@ -25,34 +76,42 @@ function App() {
         {screen === 'main' ? (
           <div className='w-full border grid grid-cols-2 gap-2 p-4 mt-4'>
             <button
-              id='scanner'
-              name='Scanner,Scan Barcode'
-              className='btn btn-lg btn-primary text-white'
+              id='receiving'
+              name='Receiving,Incoming Merchandise'
+              className='btn btn-lg btn-error text-white'
               onClick={handleClick}
             >
-              Scanner
-            </button>
-            <button
-              id='scanner'
-              name='Future Button,Future Subtitle'
-              className='btn btn-lg btn-primary text-white'
-            >
-              Future Button
+              Receiving
             </button>
           </div>
+        ) : screen === 'receiving' ? (
+          <>
+            <BackButton />
+            <HomeButton />
+            <div className='w-full border grid grid-cols-2 gap-2 p-4 mt-4'>
+              <button
+                id='scanner'
+                name='Scanner,Scan Barcode'
+                className='btn btn-lg btn-error text-white'
+                onClick={handleClick}
+                disabled={!isMobile}
+              >
+                {!isMobile ? 'Mobile Only' : 'Scanner'}
+              </button>
+              <button
+                id='scanner'
+                name='Future Button,Future Subtitle'
+                className='btn btn-lg btn-outline btn-error text-white'
+              >
+                Future Button
+              </button>
+            </div>
+          </>
         ) : (
           screen === 'scanner' && (
             <>
-              <div className='absolute top-4 left-4'>
-                <button
-                  id='main'
-                  name='BIM,BoxValet Inventory Management'
-                  className='btn btn-circle btn-primary text-white'
-                  onClick={handleClick}
-                >
-                  &lt;
-                </button>
-              </div>
+              <BackButton />
+              <HomeButton />
               <div className='mt-4'>
                 <Scanner2 />
                 <p className='mt-4'>Code: {barCode}</p>
--- src/Scanner2.jsx
+++ src/Scanner2.jsx
@@ -1,71 +1,52 @@
-import { Html5Qrcode } from 'html5-qrcode';
+import { Html5Qrcode, Html5QrcodeSupportedFormats } from 'html5-qrcode';
+import { isMobile } from 'react-device-detect';
 import useBarCodeStore from './store';
 
 const Scanner2 = () => {
   const setBarCode = useBarCodeStore((state) => state.setBarCode);
+  if (isMobile) {
+    Html5Qrcode.getCameras() // Trigger user permissions
+      .then((devices) => {
+        if (devices && devices.length) {
+          const html5QrCode = new Html5Qrcode('reader');
 
-  // This method will trigger user permissions
-  Html5Qrcode.getCameras()
-    .then((devices) => {
-      /**
-       * devices would be an array of objects of type:
-       * { id: "id", label: "label" }
-       */
-      if (devices && devices.length) {
-        // var cameraId = devices[0].id;
-        const html5QrCode = new Html5Qrcode('reader');
+          const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+            console.log(decodedResult);
+            setBarCode(decodedText);
+            html5QrCode
+              .stop()
+              .then(() => {
+                console.log('Scanning Complete');
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          };
 
-        const qrCodeSuccessCallback = (decodedText, decodedResult) => {
-          console.log(decodedResult);
-          setBarCode(decodedText);
-          html5QrCode
-            .stop()
-            .then(() => {
-              // QR Code scanning is stopped.
-              console.log('Scanning Complete');
-            })
-            .catch((err) => {
-              // Stop failed, handle it.
-              console.log(err);
-            });
-        };
+          const config = { fps: 10, qrbox: { width: 250, height: 250 } };
 
-        const config = { fps: 10, qrbox: { width: 250, height: 250 } };
+          html5QrCode.start(
+            { facingMode: 'environment' },
+            { formatsToSupport: [Html5QrcodeSupportedFormats.CODE_39] },
+            config,
+            qrCodeSuccessCallback
+          );
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
 
-        html5QrCode.start(
-          { facingMode: 'environment' },
-          config,
-          qrCodeSuccessCallback
-        );
-
-        // html5QrCode
-        //   .start(
-        //     cameraId,
-        //     {
-        //       fps: 10, // Optional, frame per seconds for qr code scanning
-        //       qrbox: { width: 250, height: 250 }, // Optional, if you want bounded box UI
-        //     },
-        //     (decodedText, decodedResult) => {
-        //       // do something when code is read
-        //       console.log(decodedResult);
-        //       setBarCode(decodedText);
-        //     },
-        //     (errorMessage) => {
-        //       // parse error, ignore it.
-        //       console.log(errorMessage);
-        //     }
-        //   )
-        //   .catch((err) => {
-        //     // Start failed, handle it.
-        //     console.error(err);
-        //   });
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-
-  return <div id='reader' width='600px'></div>;
+  return (
+    <>
+      {!isMobile ? (
+        <h1>Scanner optimized for mobile only</h1>
+      ) : (
+        <div id='reader' width='600px'></div>
+      )}
+    </>
+  );
 };
 
 export default Scanner2;
